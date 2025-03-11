994e1b8cb06fed1abdc2141a5834fb68684b699c
Renata Shirlen <renata.shirlen@gmail.com>
2025-03-11 19:07:13 -0300

    Adição do código

--- lab-03.py
+++ lab-03.py
@@ -4,3 +4,10 @@
 # Passo 1: Criar uma variável
 # Passo 2: Criar uma estrutura de condição para verificar a idade
 # Passo 3: Imprimir o resultado adequadamente de acordo com a idade da pessoa
+
+def maioridade():
+    idade = int(input("Digite a idade da pessoa: "))
+    if idade >= 18:
+        print("A pessoa é maior de idade")
+    else: 
+        print("A pessoa é menor de idade")
